070aaf549246f0106790f480b1d7ff0072558199
YUL HUR <59783267+YUL0426@users.noreply.github.com>
2021-08-26 15:10:18 +0900

    Create 투자 귀재 규식이.py

---
+++ 알고리즘정석/문제해결능력기르기/LEVEL1/투자 귀재 규식이.py
@@ -0,0 +1,22 @@
+def sublist_max(profits):
+    max_profit = profits[0]  # 최대 수익, 받는 리스트의 첫 번째 값을 일단 초기 설정
+
+    for i in range(len(profits)):
+        # 인덱스 i부터 j까지 수익의 합을 보관하는 변수
+        total = 0
+
+        for j in range(i, len(profits)):
+          #i부터 리스트를 또 돈다.
+            # i부터 j까지 수익의 합을 계산
+            total += profits[j]
+
+            # i부터 j까지 수익의 합이 최대 수익이라면, max_profit 업데이트
+            max_profit = max(max_profit, total)
+
+    return max_profit
+
+
+# 테스트
+print(sublist_max([4, 3, 8, -2, -5, -3, -5, -3]))
+print(sublist_max([2, 3, 1, -1, -2, 5, -1, -1]))
+print(sublist_max([7, -3, 14, -8, -5, 6, 8, -5, -4, 10, -1, 8]))
