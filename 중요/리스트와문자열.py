bd94c43f8dc0168fd6701c4a73dd453e6ce4286b
YUL HUR <59783267+YUL0426@users.noreply.github.com>
2021-09-04 21:00:59 +0900

    Create 리스트와문자열.py

---
+++ 중요/리스트와문자열.py
@@ -0,0 +1,15 @@
+코드2)
+alphabet_list=['a','b','c','d','e','f','g','h','i','j']
+
+print(alphabet_list[0:5])
+print(alphabet_list[4:])
+print(alphabet_list[1:])
+print(alphabet_list[:1])
+print(alphabet_list[:4])
+
+*출력결과
+['a', 'b', 'c', 'd', 'e']
+['e', 'f', 'g', 'h', 'i', 'j']
+['b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+['a']
+['a', 'b', 'c', 'd']
