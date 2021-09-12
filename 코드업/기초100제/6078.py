000fd2a07777dbc45642a3c752b2eac2e1b2da6a
YUL HUR <59783267+YUL0426@users.noreply.github.com>
2021-09-12 13:16:48 +0900

    Create 6078.py

---
+++ 코드업/기초100제/6078.py
@@ -0,0 +1,18 @@
+내코드)
+n=input()
+
+while n!='q':
+ print(n)
+ n=input()
+ if n=='q':
+  break
+  
+다른코드)
+while True:
+     x=input()
+     print(x)
+     if x=='q':
+          break
+        
+입력받은 문자를 계속 저장하여 한번에 출력해야하는거 아닌가...?
+그래서 그렇기 위해선 리스트에 받기를 해야하는 거 아닌가...?
