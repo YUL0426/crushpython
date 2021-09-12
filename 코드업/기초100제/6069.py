fed693a185a834c4f2033205306d339b4575ccb3
YUL HUR <59783267+YUL0426@users.noreply.github.com>
2021-09-12 12:14:13 +0900

    Create 6069.py

---
+++ 코드업/기초100제/6069.py
@@ -0,0 +1,27 @@
+내코드)자꾸 오류뜸 line5에서
+score = input()
+
+if score == 'A':
+    print(best!!!)
+elif score == 'B':
+    print(good!!)
+elif score == 'C':
+    print(run!)
+elif score == 'D':
+    print(slowly~)
+else:
+    print(what?)
+    
+모범답안)
+a=input()
+if  a=='A':
+    print('best!!!')
+elif a=='B':
+    print('good!!')
+elif a=='C':
+    print('run!')
+elif a=='D':
+    print('slowly~')
+else : print('what?')
+
+#뭐가다른거지...ㅋㅋㅋ
