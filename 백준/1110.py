de16ef19e2b6b842d880a65dbc6222214348ce5a
YUL HUR <59783267+YUL0426@users.noreply.github.com>
2021-09-16 08:15:49 +0900

    Update 1110.py

--- 백준/1110.py
+++ 백준/1110.py
@@ -61,3 +61,18 @@ while 1:
     if num == n:
         print(cnt)
         break
+
+ 코드4) 위의 1,2와 뭔차이가 있는지 모르겠지만 이건 pass함
+num = int(input())
+check = num
+new_num = 0
+temp = 0
+count = 0
+while True:
+    temp = num//10 + num%10
+    new_num = (num%10)*10 + temp%10
+    count += 1
+    num = new_num
+    if new_num == check:
+        break
+print(count)
