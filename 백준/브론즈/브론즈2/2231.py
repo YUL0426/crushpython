fc8a5a66b2a16c00ac0c2c668b05c927fa5b8840
YUL HUR <59783267+YUL0426@users.noreply.github.com>
2021-09-18 20:40:01 +0900

    Create 2231.py
    a = list(map(int, str(i))) 
    result = i + sum(a)

---
+++ 백준/브론즈/브론즈2/2231.py
@@ -0,0 +1,16 @@
+코드)
+n = int(input())
+result = 0
+
+for i in range(1, n+1):
+ a = list(map(int, str(i)))
+ #리스트를 통해 i의 각 자릿수를 a리스트에 넣는다
+ result = i + sum(a)
+ #분해합이 256(245+2+4+5)이므로 문제에 제시된 대로 그대로인 값 i와 각 자리수가 들어간 리스트 a 리스트 값의 합을 더하면 된다
+ if result == n:
+  print(i)
+  break
+
+ if i==n:
+  print(0)
+  #생성자가 없을경우 0 출
