a51a55e0b769f77c795a7e38220ed8ea9582f650
YUL HUR <59783267+YUL0426@users.noreply.github.com>
2021-09-18 21:22:13 +0900

    Update and rename 백준/1065.py to 백준/실버/실버4/1065(한수).py

--- 백준/실버/실버4/1065(한수).py
+++ 백준/실버/실버4/1065(한수).py
@@ -1,3 +1,19 @@
+210918코드)
+n = int(input())
+cnt = 0
+
+for i in range(1, n+1):
+ if i <= 99:
+  cnt += 1
+
+ else:
+  nums = list(map(int, str(i)))
+  if nums[0] - nums[1] == nums[1] - nums[2]:
+   cnt += 1
+
+print(cnt)
+
+210914코드)
 num = int(input())
 hansu = 0
 
