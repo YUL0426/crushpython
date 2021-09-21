257ec9f445cce0fc4af453a3b071c780af42bbe0
YUL HUR <59783267+YUL0426@users.noreply.github.com>
2021-09-21 19:26:58 +0900

    Create 2750(수 정렬하기).py

---
+++ 백준/브론즈/브론즈1/2750(수 정렬하기).py
@@ -0,0 +1,19 @@
+
+
+210921)
+
+코드1)파이썬 라이브러리 활용
+n = int(input())
+list = []
+
+for i in range(n):
+ list.append(int(input()))
+
+list = sorted(list)
+
+for i in range(len(list)):
+ print(list[i])
+
+#append를 까먹고 있었다 매일 역시 코딩해야한다.
+리스트 = sorted(리스트)
+
