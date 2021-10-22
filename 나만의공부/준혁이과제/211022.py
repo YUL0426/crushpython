8b2f1660536e9bf470039b0772dd3cab8d1e098b
YUL HUR <59783267+YUL0426@users.noreply.github.com>
2021-10-22 12:27:24 +0900

    Update 211022.py

--- 나만의공부/준혁이과제/211022.py
+++ 나만의공부/준혁이과제/211022.py
@@ -5,21 +5,27 @@ backpack = [[],[0]] #[[courses],[total credits]]
 
 for i in range(len(course)):
     print(i, course[i])
-tmp=0
+tmp = -99 // 중복방지를 위한 변수설정, while문 안은 지역변수 이므로 전역변수로 설정해 주어야함***
 
 while 1:
-    n = int(input())
-    if -1 < n < 10 and backpack[1][0] <=20:
+    n = int(input()) // 수강신청입력받기위함
+    if -1 < n < 10 and n==tmp and backpack[1][0] <= 20:           //중복시 거르는 조건
+        print("you already have that")
+    if -1 < n < 10 and n!=tmp and backpack[1][0] <= 20:
         backpack[0].append(course[n][0])
         backpack[1][0] = backpack[1][0] + course[n][1]
         print(backpack)
-    if 12 <= backpack[1][0] <=20:
+    if n != -1 and 12 <= backpack[1][0] <20:
         print("you can add", 20-backpack[1][0], "more credit")
-    if backpack[1][0] > 20:
+    elif backpack[1][0] > 20:
         print("you put too much")
-    elif n==-1 and backpack[1][0] < 15:
+    if n == -1 and backpack[1][0] >= 15:
+        print(backpack)
+        break
+    if n == -1 and backpack[1][0] < 15:
         print(backpack)
         print("you have to pick more subject next time")
         break
-    elif n <-1 and n >= 10:
+    elif n <-1 or n >= 10:
         print("error")
+    tmp = n
