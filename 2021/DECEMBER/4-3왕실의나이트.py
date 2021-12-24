3755ae3de3ea8e7a5619e8e5f17e31a5019b06a8
YUL HUR <59783267+YUL0426@users.noreply.github.com>
2021-12-24 12:29:55 +0900

    Update 4-3왕실의나이트.py

--- 2021/DECEMBER/4-3왕실의나이트.py
+++ 2021/DECEMBER/4-3왕실의나이트.py
@@ -19,3 +19,20 @@ for step in steps:
         result += 1
 
 print(result)
+
+#두번쨰 풀이 https://velog.io/@suzieep/Algorithm-이코테-왕실의-나이트-파이썬
+dot = list(input())
+
+x = ord(dot[0])-96
+y = int(dot[1])
+cnt = 0
+
+dx = [-1,1,2,2,1,-1,-2,-2]
+dy = [2,2,1,-1,-2,-2,-1,1]
+for i in range(len(dx)):
+    nx = x+dx[i]
+    ny = y+dy[i]
+    if nx<=8 and nx>=1 and ny>=1 and ny<=8:
+        cnt+=1
+
+print(cnt)
